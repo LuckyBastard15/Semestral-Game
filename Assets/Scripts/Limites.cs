b5629db10918a400d3165a35455be25f115c0031
LuckyBastard15 <85241539+LuckyBastard15@users.noreply.github.com>
2021-10-28 10:08:29 -0500

    upload game

--- Assets/Scripts/Limites.cs
+++ Assets/Scripts/Limites.cs
@@ -9,6 +9,7 @@ public class Limites : MonoBehaviour
     void Start()
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        
     }
     
     // Update is called once per frame
